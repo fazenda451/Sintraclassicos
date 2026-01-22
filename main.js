a1e71015e450856f3aeee1afd79e871fc29f462e
Rodrigo Fazenda <152302205+fazenda451@users.noreply.github.com>
2026-01-22 14:29:13 +0000

    mudança do email no main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function inicializarInteracoes() {
         const formData = new FormData(comunidadeForm);
         
         // Enviar para FormSubmit
-        const response = await fetch('https://formsubmit.co/ajax/rodrigo.m.fazenda@gmail.com', {
+        const response = await fetch('https://formsubmit.co/ajax/sintraclassicos14@gmail.com', {
           method: 'POST',
           body: formData
         });
@@ -131,7 +131,7 @@ function inicializarInteracoes() {
         const formData = new FormData(contactoForm);
         
         // Enviar para FormSubmit
-        const response = await fetch('https://formsubmit.co/ajax/rodrigo.m.fazenda@gmail.com', {
+        const response = await fetch('https://formsubmit.co/ajax/sintraclassicos14@gmail.comm', {
           method: 'POST',
           body: formData
         });
